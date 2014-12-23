ab790feccc45b46b42bf604a3920e9a7c26db169
smith750 <smith750@iu.edu>
2014-12-23 17:30:07 -0500

    First commit, we haven't gotten too far

---
+++ TextRetrieval.hs
@@ -0,0 +1,22 @@
+-- much stealing from http://adit.io/posts/2012-03-10-building_a_concurrent_web_scraper_with_haskell.html
+
+import Network.HTTP
+import Network.URI
+import Control.Concurrent.ParallelIO
+import Data.Char
+import Control.Monad.Trans
+import Control.Monad.Maybe
+
+-- helper function for getting page content
+openUrl :: String -> MaybeT IO String
+openUrl url = case parseURI url of
+    Nothing -> fail ""
+    Just u  -> liftIO (getResponseBody =<< simpleHTTP (mkRequest GET u))
+	
+--urls = ["http://gutenberg.pglaf.org/4/7/6/5/47651/47651-0.txt","http://gutenberg.pglaf.org/1/4/0/1400/1400.txt"]
+	
+main = do
+	doc <- runMaybeT $ openUrl "http://gutenberg.pglaf.org/1/4/0/1400/1400.txt"
+	wordCount <- length $ map toLower words doc
+--	putStrLn wordCount
+	return ()
---
+++ readme.md
@@ -0,0 +1,1 @@
+Simple program to try out asynchronous code in Haskell.
